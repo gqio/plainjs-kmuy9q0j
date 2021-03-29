af94e1981b9afb97b0d5c5120b22d2d5ecacf71b
Gregory Quinio <quinio.gregory@gmail.com>
2021-03-29 20:08:44 +0000

    deploy Mon Mar 29 2021 20:08:43 GMT+0000 (Coordinated Universal Time)

---
+++ dist/index.stories.js
@@ -0,0 +1,19 @@
+import "./index.js";
+export default {
+  parameters: {
+    layout: "centered"
+  }
+};
+export const story1 = () => `<my-counter></my-counter>`;
+export const __LOCATIONS_MAP__ = {
+  "story1": {
+    "startLoc": {
+      "line": 9
+    },
+    "endLoc": {
+      "line": 9
+    }
+  }
+};
+export const __SOURCE_FILE__ = "src/index.stories.js";
+export const __namedExportsOrder = ["story1"];
